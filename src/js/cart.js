653c177981e65e3f4c930eeac82d0433813f3cd8
Felipe <felipesantos.dsb@hotmail.com>
2023-01-07 22:27:16 -0300

    fix adding items to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,12 @@
-import { getLocalStorage } from "./utils.mjs";
+import {
+  getLocalStorage
+} from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const itemArray = [];
+  itemArray.push(cartItems);
+  const htmlItems = itemArray.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 

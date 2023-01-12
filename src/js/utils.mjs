ac31271042f449af32ecd77f5fac561070beaf65
willardnyamombe <willardnya@gmail.com>
2023-01-12 14:33:25 -0700

    add to cart added

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,8 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+// Willard Tries
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {

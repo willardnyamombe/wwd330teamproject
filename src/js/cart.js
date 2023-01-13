05c399e2767cae9e2365b6cc719d6907646ea1b8
willardnyamombe <willardnya@gmail.com>
2023-01-12 18:49:58 -0700

    fixed the error

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,3 +1,4 @@
+import { getLocalStorage } from "./utils.mjs";
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));

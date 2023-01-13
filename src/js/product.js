680c557b6b5aae210425dd90bf34f0cdc3b7b9f1
willardnyamombe <willardnya@gmail.com>
2023-01-12 20:48:25 -0700

    team done deal

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+const productId = getParam("product");
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart");
-  if(cart === null){
-      cart = [];
-  }
-  cart.push(product)
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();

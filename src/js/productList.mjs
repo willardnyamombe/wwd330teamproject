78d2ce174f6a5e587514a73f813d7039d9deb654
willardnyamombe <willardnya@gmail.com>
2023-01-23 21:03:25 -0700

    match

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,49 +1,40 @@
 import { renderListWithTemplate } from "./utils.mjs";
-//import ProductData from "./ProductData.mjs";
 
 function productCardTemplate(product) {
-    //Use destructoring to enable readable code and pull specific properties from our object
-    const {Id, Image, ListPrice, NameWithoutBrand} = product;
-    const {Name} = product.Brand;
-
-    return `<li class="product-card">
-    <a href="product_pages/?product=${Id}">
-      <img
-        src="${Image}"
-        alt="Image of ${NameWithoutBrand}"
-      />
-      <h3 class="card__brand">${Name}</h3>
-      <h2 class="card__name">${NameWithoutBrand}</h2>
-      <p class="product-card__price">$${ListPrice}</p></a>
-  </li>`
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
 }
 
-// class to generate list of product cards
-// in HTML from an array.
-export default class productListing {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    }
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    // We passed in this information to make our class as reusable as possible.
+    // Being able to define these things when we use the class will make it very flexible
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+  async init() {
+    // our dataSource will return a Promise...so we can use await to resolve it.
+    const list = await this.dataSource.getData();
+    // render the list
+    this.renderList(list);
+  }
+  // render after doing the first stretch
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 
-    async init() {
-        let productsArray = await this.dataSource.getData();
-        let filterdArray = this.filterProductList(productsArray);
-        // console.log(filterdArray);
-        this.renderList(productCardTemplate, filterdArray);
-    }
-
-    renderList(template, list) {
-        renderListWithTemplate(template, this.listElement, list)
-    }
-
-    filterProductList(list) {
-      // return random list of product whose index is less than 4 
-      // from the list of all tents/products.
-      var randomNum = Math.floor(Math.random() * ((list.length + 1) - 4));
-      return list.slice(randomNum, randomNum + 4);
-
-      
-    }   
+  // render before doing the stretch
+  // renderList(list) {
+  //   const htmlStrings = list.map(productCardTemplate);
+  //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+  // }
 }
